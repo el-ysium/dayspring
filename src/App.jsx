f05350b7142571ebb04f4a26644e1d4543c94cec
Angel <angel@elysiums-MacBook-Pro.local>
2024-01-15 00:20:30 +0100

    deploy

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+function App() {
+
+  return (
+    <>
+      <section className="bg-gray-900 pb-96 text-white">
+        <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
+          <div className="mx-auto max-w-3xl text-center">
+            <h1
+              className="bg-gradient-to-r pb-5 from-green-300 via-blue-500 to-purple-600 bg-clip-text text-3xl font-extrabold text-transparent sm:text-5xl"
+            >
+              DaySpring OnHigh
+              <span className="sm:block"> Web Design Classes </span>
+            </h1>
+
+            <p className="mx-auto mt-4 max-w-xl sm:text-xl/relaxed">
+              Get files and resources for your classes by clicking the button for your respective classes
+            </p>
+
+            <div className="mt-8 block justify-items-center space-y-5">
+              <a
+                className="block w-full mx-auto rounded border border-blue-600 px-12 py-3 text-md font-medium text-white hover:bg-blue-600 focus:outline-none focus:ring active:bg-blue-500 sm:w-1/2"
+                href="https://drive.google.com/drive/folders/11kB2OR8aMnG1YORs3DrKkpyBJK5TXMty?usp=sharing"
+              >
+                Pry 1-5
+              </a>
+
+              <a
+                className="block w-full mx-auto rounded border border-blue-600 px-12 py-3 text-md font-medium text-white hover:bg-blue-600 focus:outline-none focus:ring active:bg-blue-500 sm:w-1/2"
+                href="https://drive.google.com/drive/folders/1CQS9fSq5XoxaXOTi1D1qOjfzl0I1EVRb?usp=sharing"
+              >
+                JSS 1-3
+              </a>
+              <a
+                className="block w-full mx-auto rounded border border-blue-600 px-12 py-3 text-md font-medium text-white hover:bg-blue-600 focus:outline-none focus:ring active:bg-blue-500 sm:w-1/2"
+                href="https://drive.google.com/drive/folders/1vzH65MFkOusUwyaIjYvsk45c7FU5UIdP?usp=sharing"
+              >
+                SSS 1-2
+              </a>
+              <a
+                className="block w-full mx-auto rounded border border-blue-600 px-12 py-3 text-md font-medium text-white hover:bg-blue-600 focus:outline-none focus:ring active:bg-blue-500 sm:w-1/2"
+                href="https://drive.google.com/drive/folders/1W0lxR-6kACKpy_FRDN76qFv3a5SYkPIw?usp=sharing"
+              >
+                SSS 3
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default App
